c773b3ce866c94f1cccfbf1a7db5ba3a9ac87da1
Teo Beltran <teo.belnan@gmail.com>
2024-06-16 00:40:51 -0300

    ++

---
+++ Login/Usuario.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Login
+{
+    partial class Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblU2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(174, 112);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(43, 13);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "Usuario";
+            // 
+            // lblU2
+            // 
+            this.lblU2.AutoSize = true;
+            this.lblU2.Location = new System.Drawing.Point(174, 146);
+            this.lblU2.Name = "lblU2";
+            this.lblU2.Size = new System.Drawing.Size(13, 13);
+            this.lblU2.TabIndex = 3;
+            this.lblU2.Text = "_";
+            // 
+            // Usuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblU2);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Usuario";
+            this.Text = "Usuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblU2;
+    }
+}
