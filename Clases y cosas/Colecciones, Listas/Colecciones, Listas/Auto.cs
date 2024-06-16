710ac7e4bc360677e409edd426530a211dd8a9c4
Teo Beltran <teo.belnan@gmail.com>
2024-06-16 00:48:14 -0300

    ++

---
+++ Clases y cosas/Colecciones, Listas/Colecciones, Listas/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones__Listas
+{
+    internal class Auto : Vehiculo, Deportivo
+    {
+        public string carreras()
+        {
+            return "si";
+        }
+    }
+}
---
+++ Clases y cosas/POO1b (articulo)/ejemplo2/Articulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    internal class Articulo
+    {
+        public int CodigoArticulo { get; set; }
+        
+        public float Precio { get; set; }
+
+        private int codMarca;
+        public int CodigoMarca
+        {
+            get { return codMarca; }
+            set
+            {
+                if (value > 0 && value < 11)
+                    codMarca = value;
+                else
+                    codMarca = -1;
+            }
+        }
+    }
+}
