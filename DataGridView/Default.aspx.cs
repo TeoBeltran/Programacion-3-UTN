507bdd7c1b7d814a9c1ba1f7de1f2a2b1df431d7
Teo Beltran <teo.belnan@gmail.com>
2024-06-16 00:47:36 -0300

    +

---
+++ DataGridView/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using DataGridView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataGridView
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["listaAutos"] == null)
+            {
+                AutoNegocio negocio = new AutoNegocio();
+                Session.Add("listaAutos", negocio.listar());
+            }
+
+            dgvAutos.DataSource = Session["listaAutos"];    
+            dgvAutos.DataBind();
+        }
+    }
+}
---
+++ EjemploMaxi/Contact.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP4
+{
+    public partial class Contact : Page
+    {
+        public string user { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            user = Session["usuario"] != null ? Session["usuario"].ToString() : "";
+
+            lblTeo.Text = user;
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            user = Session["usuario"] != null ? Session["usuario"].ToString() : "";
+
+            lblTeo.Text = user + ", tu ingreso fue exitoso.";
+        }
+
+        protected void textBoxTeo_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
